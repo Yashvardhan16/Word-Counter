daf6b3ef894597ff6c4b51eae7caf66fbac1a523
Yashvardhan16 <84308540+Yashvardhan16@users.noreply.github.com>
2021-07-10 17:15:39 +0530

    Add files via upload

---
+++ count.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $("#word_count").on('keyup', function() {
+    var words = 0;
+
+    if ((this.value.match(/\S+/g)) != null) {
+      words = this.value.match(/\S+/g).length;
+    }
+
+    if (words > 200) {
+      // Split the string on first 200 words and rejoin on spaces
+      var trimmed = $(this).val().split(/\s+/, 200).join(" ");
+      // Add a space at the end to make sure more typing creates new words
+      $(this).val(trimmed + " ");
+    }
+    else {
+      $('#display_count').text(words);
+      $('#word_left').text(200-words);
+    }
+  });
+});
